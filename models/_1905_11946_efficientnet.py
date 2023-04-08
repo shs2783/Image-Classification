a7e33472533c3186526efada4904a22fa5d33e0d
shs2783 <shs2783_@naver.com>
2023-04-09 00:05:25 +0900

    update EfficientNet MBConvBlock and SEBlock reduction channels

--- models/_1905_11946_efficientnet.py
+++ models/_1905_11946_efficientnet.py
@@ -36,13 +36,12 @@ class DepthwiseConv2d(ConvBlock):  # conv3x3
 
 
 class SEBlock(nn.Module):
-    def __init__(self, channels, reduction_ratio=16) -> None:
+    def __init__(self, channels, reduction_channels) -> None:
         super().__init__()
 
-        reduction_channels = math.ceil(channels / reduction_ratio)
         self.squeeze_excitation_layers = nn.Sequential(
             nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(channels, reduction_channels, kernel_size=1),  # same as nn.Linear(gate_channels, channels),
+            nn.Conv2d(channels, reduction_channels, kernel_size=1),  # same as nn.Linear(gate_channels, channels)
             nn.ReLU(),
             nn.Conv2d(reduction_channels, channels, kernel_size=1),
             nn.Sigmoid()
@@ -52,17 +51,18 @@ class SEBlock(nn.Module):
         return x * self.squeeze_excitation_layers(x)
     
 class MBConvBlock(nn.Module):  # InvertedResidual + SE
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, expansion=6, reduction_ratio=16) -> None:
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, expansion=6, reduction_ratio=4) -> None:
         super().__init__()
 
         self.residual = (in_channels == out_channels)
         hidden_channels = expansion * in_channels
+        reduction_channels = int(in_channels / reduction_ratio)
 
         self.bottleneck_layer = nn.Sequential(
             PointwiseConv2d(in_channels, hidden_channels),
             DepthwiseConv2d(hidden_channels, hidden_channels, kernel_size, stride=stride, padding=padding),
+            SEBlock(hidden_channels, reduction_channels),
             PointwiseConv2d(hidden_channels, out_channels),
-            SEBlock(out_channels, reduction_ratio),
         )
 
     def forward(self, x):
@@ -75,10 +75,7 @@ class MBConvBlock(nn.Module):  # InvertedResidual + SE
             return scaled_feature
     
 class EfficientNet(nn.Module):
-    # TODO 1: stochastic depth with survival probability 0.8
-    # TODO 2: MnasNet ConvBlock
-
-    def __init__(self, config=efficientnet_architecture, depth_multiplier=1.2, width_multiplier=1.1, reduction_ratio=16, in_channels=3, num_classes=1000) -> None:
+    def __init__(self, config=efficientnet_architecture, depth_multiplier=1.2, width_multiplier=1.1, reduction_ratio=4, in_channels=3, num_classes=1000) -> None:
         super().__init__()
 
         self.conv1_channels = conv1_channels = math.ceil(32 * width_multiplier)
@@ -87,10 +84,11 @@ class EfficientNet(nn.Module):
         self.conv_layer1 = ConvBlock(in_channels=in_channels, out_channels=conv1_channels, kernel_size=3, stride=2, padding=1)
         self.mobile_se_conv_layers, out_channels = self._create_layers(config, reduction_ratio, depth_multiplier, width_multiplier)
         self.conv_layer2 = ConvBlock(in_channels=out_channels, out_channels=conv2_channels, kernel_size=1)
+
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.conv_layer3 = nn.Conv2d(in_channels=conv2_channels, out_channels=num_classes, kernel_size=1)
 
-    def _create_layers(self, architecture, reduction_ratio=16, depth_multiplier=1.2, width_multiplier=1.1):
+    def _create_layers(self, architecture, reduction_ratio=4, depth_multiplier=1.2, width_multiplier=1.1):
         layers = []
         in_channels = self.conv1_channels
         
@@ -109,18 +107,22 @@ class EfficientNet(nn.Module):
         x = self.conv_layer1(x)
         x = self.mobile_se_conv_layers(x)
         x = self.conv_layer2(x)
+        
         x = self.avg_pool(x)
         x = self.conv_layer3(x)
         x = x.flatten(start_dim=1)
         return x
 
 if __name__ == '__main__':
+    # TODO 1: stochastic depth with survival probability 0.8
+    # TODO 2: calculate detph, width, resolution multiplier
+
     compound_coefficient = 0
     depth_multiplier = 1.2 ** compound_coefficient
     width_multiplier = 1.1 ** compound_coefficient
     resolution_multi = 1.15 ** compound_coefficient
     
-    model = EfficientNet(depth_multiplier=depth_multiplier, width_multiplier=width_multiplier, reduction_ratio=16)
+    model = EfficientNet(depth_multiplier=depth_multiplier, width_multiplier=width_multiplier, reduction_ratio=4)
 
     resolution = int(resolution_multi * 224)
     x = torch.randn(1, 3, resolution, resolution)
